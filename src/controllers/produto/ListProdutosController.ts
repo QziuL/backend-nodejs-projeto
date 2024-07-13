ab9e77216ded4082421964d38e73baff4738435e
QziuL <lf.quinholi2@gmail.com>
2024-07-12 23:49:12 -0300

    fix: retorna lista mesmo vazia

--- src/controllers/produto/ListProdutosController.ts
+++ src/controllers/produto/ListProdutosController.ts
@@ -5,9 +5,10 @@ export default class ListProdutosController {
   async handle(res: Response) {
     try {
       const produtos = await new ListProdutosService().execute();
-      produtos.length
-        ? res.status(200).json(produtos)
-        : res.json({ msg: "Puxa vida, estamos sem produtos..." });
+      return res.status(200).json(produtos);
+      // produtos.length
+      //   ? res.status(200).json(produtos)
+      //   : res.json({ msg: "Puxa vida, estamos sem produtos..." });
     } catch (error) {
       if (error instanceof Error) {
         res.status(500).json({ error: error.message });
